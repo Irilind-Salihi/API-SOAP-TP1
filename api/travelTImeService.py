93f47aa9af745e47d036a54c562a2bedc7be495c
Irilind-Salihi <73695089+Irilind-Salihi@users.noreply.github.com>
2023-03-16 05:56:27 +0100

    Update travelTImeService.py

--- api/travelTImeService.py
+++ api/travelTImeService.py
@@ -1,29 +1,31 @@
-from spyne import Application, rpc, ServiceBase, \
-    Integer, Unicode , Decimal, Boolean
-from spyne import Iterable
+from spyne import Application, rpc, ServiceBase, Decimal
 from spyne.protocol.soap import Soap11
 from spyne.server.wsgi import WsgiApplication
-import decimal
 from wsgiref.simple_server import make_server
 
 
-class TravelService(ServiceBase):               
+class CalculService(ServiceBase):
 
-    @rpc(Unicode, Integer, _returns=Iterable(Unicode))
-    def say_hello(ctx, name, times):
-        for i in range(times):
-            yield u'Hello, %s' % name
+    @rpc(Decimal, Decimal, Decimal, Decimal, _returns=Decimal)
+    def calculer_temps_trajet(ctx, distance_km, vitesse_km_h, nb_recharge, temps_recharge_h):
+        ctx.transport.resp_headers['Access-Control-Allow-Origin'] = '*'
+        
+        # Temps de trajet sans recharge
+        temps_trajet = distance_km / vitesse_km_h  
+        
+        # Temps total de recharge
+        temps_recharge_total = nb_recharge * temps_recharge_h
 
-    @rpc(Decimal, Decimal, Decimal, Boolean, _returns=Decimal)
-    def travelTime(ctx,  distance, speed):
-        ctx.transport.resp_headers['Access-Control-Allow-Origin'] = 'https://voiture-calculator.vercel.app'
-        total_time = distance/speed
-        return total_time
+        # Temps total de trajet avec recharges
+        temps_trajet_total = temps_trajet + temps_recharge_total
+
+        return temps_trajet_total
 
 
-application = Application([TravelService], 'spyne.examples.traject.soap',
-                          in_protocol=Soap11(validator='lxml'),
-                          out_protocol=Soap11())
+application = Application([CalculService], 'info.802.calcul.soap',
+    in_protocol=Soap11(validator='lxml'),
+    out_protocol=Soap11()
+)
 
 wsgi_application = WsgiApplication(application)
 
