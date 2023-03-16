ac225b4700ccdf8cf0762126e440e1813ef75b22
Irilind-Salihi <73695089+Irilind-Salihi@users.noreply.github.com>
2023-03-16 04:10:27 +0100

    Update for vercel

--- api/travelTImeService.py
+++ api/travelTImeService.py
@@ -7,17 +7,11 @@ from spyne import Iterable
 
 
 class TravelService(ServiceBase):
-    @rpc(Unicode, Integer, _returns=Iterable(Unicode))
-    def say_hello(ctx, name, times):
-        for i in range(times):
-            yield u'Hello, %s' % name
 
     @rpc(Integer, Integer, _returns=Iterable(Integer))
-    def travelTime(ctx, autonomy, distance, speed, reload_time):
-        time_to_travel = distance / speed
-        reload_count = distance / autonomy
-        time_to_reload = reload_count * reload_time
-        total_time = time_to_travel + time_to_reload
+    def travelTime(ctx,  distance, speed):
+
+        total_time = distance/speed
         yield str(total_time)
 
 
@@ -26,6 +20,3 @@ application = Application([TravelService], 'spyne.examples.hello.soap',
                           out_protocol=Soap11())
 
 wsgi_application = WsgiApplication(application)
-
-server = make_server('127.0.0.1', 8000, wsgi_application)
-server.serve_forever()
--- client.py
+++
@@ -1,7 +0,0 @@
-from zeep import Client
-wsdl = "http://127.0.0.1:8000/?wsdl"
-client = Client(wsdl=wsdl)
-print(client.service.say_hello('Zeep', 3))
-
-print(client.service.addition(2, 3))
-
