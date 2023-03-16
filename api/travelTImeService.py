55061c62d0ef65c731e79c3cdb7eeab192769475
Irilind-Salihi <73695089+Irilind-Salihi@users.noreply.github.com>
2023-03-16 05:53:58 +0100

    Update travelTImeService.py

--- api/travelTImeService.py
+++ api/travelTImeService.py
@@ -1,13 +1,20 @@
-from spyne import application
+from spyne import Application, rpc, ServiceBase, \
+    Integer, Unicode , Decimal, Boolean
+from spyne import Iterable
 from spyne.protocol.soap import Soap11
 from spyne.server.wsgi import WsgiApplication
-from spyne import Application, rpc, ServiceBase, Integer, Unicode, Decimal
+import decimal
 from wsgiref.simple_server import make_server
-from spyne import Iterable
 
 
 class TravelService(ServiceBase):               
-    @rpc(Integer, Integer, _returns=Iterable(Integer))
+
+    @rpc(Unicode, Integer, _returns=Iterable(Unicode))
+    def say_hello(ctx, name, times):
+        for i in range(times):
+            yield u'Hello, %s' % name
+
+    @rpc(Decimal, Decimal, Decimal, Boolean, _returns=Decimal)
     def travelTime(ctx,  distance, speed):
         ctx.transport.resp_headers['Access-Control-Allow-Origin'] = 'https://voiture-calculator.vercel.app'
         total_time = distance/speed
