cefb4fea96bd714f8798580ccdbe3106fecaf247
Irilind-Salihi <73695089+Irilind-Salihi@users.noreply.github.com>
2023-03-16 06:01:05 +0100

    Update apisoap.py

--- api/apisoap.py
+++ api/apisoap.py
@@ -1,32 +1,28 @@
-from spyne import Application, rpc, ServiceBase, Decimal
+from spyne import Application, rpc, ServiceBase, \
+    Integer, Unicode , Decimal, Boolean
+from spyne import Iterable
 from spyne.protocol.soap import Soap11
 from spyne.server.wsgi import WsgiApplication
+import decimal
 from wsgiref.simple_server import make_server
 
+# Le service va dire autant que fois qu'on le demande notre nom
+class HelloWorldService(ServiceBase):
+    @rpc(Unicode, Integer, _returns=Iterable(Unicode))
+    def say_hello(ctx, name, times):
+        for i in range(times):
+            yield u'Hello, %s' % name
 
-class CalculService(ServiceBase):
-
-    @rpc(Decimal, Decimal, Decimal, Decimal, _returns=Decimal)
-    def calculer_temps_trajet(ctx, distance_km, vitesse_km_h, nb_recharge, temps_recharge_h):
+    @rpc(Decimal, Decimal, Decimal, Boolean, _returns=Decimal)
+    def temps_trajet(ctx, distance, vitesse_moyenne, points, fast_charging):
         ctx.transport.resp_headers['Access-Control-Allow-Origin'] = '*'
-        
-        # Temps de trajet sans recharge
-        temps_trajet = distance_km / vitesse_km_h  
-        
-        # Temps total de recharge
-        temps_recharge_total = nb_recharge * temps_recharge_h
+        res = distance / vitesse_moyenne * 60
+        temps = 60
+        if fast_charging:
+            temps = 30 
+        return res + (points * 30)
 
-        # Temps total de trajet avec recharges
-        temps_trajet_total = temps_trajet + temps_recharge_total
-
-        return temps_trajet_total
-
-
-application = Application([CalculService], 'info.802.calcul.soap',
-    in_protocol=Soap11(validator='lxml'),
-    out_protocol=Soap11()
-)
-
+application = Application([HelloWorldService], 'spyne.examples.hello.soap',
+        in_protocol=Soap11(validator='lxml'),
+        out_protocol=Soap11())
 wsgi_application = WsgiApplication(application)
-
-app = wsgi_application
